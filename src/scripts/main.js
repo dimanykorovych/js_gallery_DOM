2969612faec05d295097ae93d5eecee0f7003c12
Dmytro Nykorovych <dev.dmytronykorovych@gmail.com>
2025-08-07 23:33:15 +0200

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const galeryLinks = document.querySelectorAll('.list-item__link');
+
+galeryLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const galeryThumb = link.querySelector('.gallery__thumb');
+
+    largeImg.setAttribute('src', galeryThumb.getAttribute('src'));
+  });
+});
