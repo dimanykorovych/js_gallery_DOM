5163b5af41a552a50b71c803ed2e9d69fc73fcbd
Dmytro Nykorovych <dev.dmytronykorovych@gmail.com>
2025-08-07 23:51:30 +0200

    make build

---
+++ index.b425311b.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector("#largeImg");document.querySelector(".gallery").addEventListener("click",function(t){var r=t.target.closest(".list-item__link");if(r){t.preventDefault();var n=r.querySelector(".gallery__thumb");n&&n.src&&(e.src=n.src)}})});
+//# sourceMappingURL=index.b425311b.js.map
